38faa45b2e58904d16575b23934f105a1907b370
Rejfi <rrafaul@wp.pl>
2020-04-06 23:22:09 +0200

    Add parsing html

--- app/src/main/java/com/example/allegrointernapp/activities/DetailActivity.kt
+++ app/src/main/java/com/example/allegrointernapp/activities/DetailActivity.kt
@@ -1,9 +1,14 @@
 package com.example.allegrointernapp.activities
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.res.FontResourcesParserCompat
 import com.example.allegrointernapp.R
 import com.example.allegrointernapp.data.Offer
 import com.squareup.picasso.Picasso
@@ -11,6 +16,8 @@ import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
+import java.lang.Exception
 
 
 class DetailActivity : AppCompatActivity() {
@@ -24,16 +31,51 @@ class DetailActivity : AppCompatActivity() {
 
         if(intent.hasExtra("offerDetails")){
             val offer = intent.getParcelableExtra<Offer>("offerDetails")!!
-
             CoroutineScope(Dispatchers.Main).launch {
-                Picasso.get().load(offer.thumbnailUrl).into(detailImage)
+                Picasso.get().load(offer.thumbnailUrl).fit().centerInside().into(detailImage)
             }
             title = offer.name
-            detailDescription.text = offer.description
             val totalPrice = "${offer.price.amount}  ${offer.price.currency}"
             detailPrice.text = totalPrice
+            prepareHtml(offer.description)
         }
+
+
+    }
+
+    private fun prepareHtml(html: String?){
+        if(!html.isNullOrEmpty()){
+            val doc = Jsoup.parse(html)
+            try{
+                val header = doc.selectFirst("h1")
+               // detailDescriptionHeader.text = header.text()
+               // detailDescriptionHeader.textSize = 25f
+                val tv = TextView(applicationContext)
+                tv.text = header.text()
+                tv.textSize = 25f
+                tv.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD_ITALIC)
+                detailContent.addView(tv)
+            }catch (e: Exception){
+                Log.d("JSOUP", e.message + "|| znacznik h2")
+            }
+            try {
+                val description = doc.select("p")
+                val tv = TextView(applicationContext)
+                tv.text = description.text()
+                tv.textSize = 20f
+                detailContent.addView(tv)
+            }catch (e: Exception){
+                Log.d("JSOUP", e.message + "|| znacznik p")
+            }
+
+        }
+    TODO("Ogarnąć parsowanie HTML aby wyświetlało w przystępnej formie znacznki odpowiednie")
+    TODO("WAŻNE Zabezpieczyć Retrofit przed błędem w pobieraniu danych będzie trzeba przebudować przepływ")
+    TODO("Przygotować layout dla wszystkich orientacji urządzenia")
+    TODO("Zaimplementować odświeżanie oferty")
+    TODO("Wprowadzić wyszukiwanie w ActionBar")
 
     }
 
 }
+
